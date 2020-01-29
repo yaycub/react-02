287d1858da19816f3e13a92959ad8cae61b3a74b
Jacob Harrington <jharrington4@gmail.com>
2020-01-29 15:45:07 -0800

    finished Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Header.css';
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className={styles.Header}>
       <h1>{title}</h1>
       <h4>a react madlib</h4>
     </header>
